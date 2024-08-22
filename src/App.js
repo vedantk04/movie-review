e58f725c438f04bb9ced21e272dc370ed85cec01
vedantk04 <vedantkumbharkar@gmail.com>
2024-08-23 01:02:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/Header';
+import Movie from './components/Movie';
+import movies from './movie.json';
+import React from 'react';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="App">
+    <Header />
+    <div className="main">
+      {
+        movies.map((element)=>{
+        return(
+         <Movie title={element.Title} 
+                year={element.Year}
+                img={element.Poster}
+                /> 
+      )  
+    })
+        
+  }
+      
     </div>
+   </div>
   );
 }
 
